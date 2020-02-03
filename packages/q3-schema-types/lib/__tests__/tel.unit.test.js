8edea6df3f147575d8af000eca98793b87acfd44
Mike <mibberson@3merge.ca>
2020-02-03 15:09:03 -0500

    fix: phone number validation to include country codes and extensions

---
+++ packages/q3-schema-types/lib/__tests__/tel.unit.test.js
@@ -0,0 +1,37 @@
+const { Schema } = require('mongoose');
+const runner = require('../utils');
+require('../tel');
+
+const M = runner(Schema.Types.Tel, [
+  ['123-903-1234', true],
+  ['(416)888-3241', true],
+  ['111.999.7183', true],
+  ['9871234325', true],
+  ['123-903-123', false],
+  ['+1 905 3211', false],
+  ['(416)     888-3241', true],
+  ['111.999..7183', true],
+  ['982271234325', true],
+  ['8001234321', true],
+  ['8001234321x1234', true],
+]);
+
+test('Type should normalize tel', () => {
+  const doc = new M({ test: '9053331234' });
+  expect(doc.test).toMatch('(905) 333-1234');
+});
+
+test('Type should normalize extensions', () => {
+  const doc = new M({ test: '9053331234x123' });
+  expect(doc.test).toMatch('(905) 333-1234 x123');
+});
+
+test('Type should normalize country codes', () => {
+  const doc = new M({ test: '19053331234' });
+  expect(doc.test).toMatch('+1 (905) 333-1234');
+});
+
+test('Type combine all conditions', () => {
+  const doc = new M({ test: '449053331234 x12' });
+  expect(doc.test).toMatch('+44 (905) 333-1234 x12');
+});
--- packages/q3-schema-types/lib/tel.js
+++ packages/q3-schema-types/lib/tel.js
@@ -10,14 +10,34 @@ Tel.prototype = Object.create(
 
 Tel.prototype.cast = function sanitize(val = '') {
   if (!val.length) return val;
-  const cleaned = `${val}`.replace(/\W|\D|\s/g, '');
-  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+  const [num, ext] = val.split('x');
+  const cleaned = `${num}`.replace(/\W|\D|\s/g, '');
+  const match = cleaned.match(
+    /^([+]?\d{1,2}[.-\s]?)?(\d{3})(\d{3})(\d{4})$/,
+  );
+
   if (!match)
     throw new Error(
       `Tel: ${val} is not a valid North American phone number`,
     );
 
-  return `(${match[1]}) ${match[2]}-${match[3]}`;
+  const [
+    ,
+    countryCode,
+    areaCode,
+    officeCode,
+    stationCode,
+  ] = match;
+
+  let formatted = '';
+  if (countryCode) formatted += `+${countryCode} `;
+  if (areaCode) formatted += `(${areaCode}) `;
+  if (officeCode) formatted += `${officeCode}-`;
+  if (stationCode) formatted += `${stationCode}`;
+  if (ext) formatted += ` x${ext}`;
+
+  return formatted;
 };
 
 Tel.prototype.castForQuery = function forward(val = '') {
