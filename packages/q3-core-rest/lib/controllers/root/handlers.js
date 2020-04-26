55cee611b6799a56dc8a6e53e6f4273740311023
Mike <mibberson@3merge.ca>
2020-04-26 11:45:59 -0400

    chore(core-rest): add collation to paginate options for improved sorting

--- packages/q3-core-rest/lib/controllers/root/handlers.js
+++ packages/q3-core-rest/lib/controllers/root/handlers.js
@@ -56,6 +56,7 @@ module.exports = {
     } = await datasource.paginate(params, {
       options: { redact: true },
       page: page >= 0 ? page + 1 : 1,
+      collation: { locale: 'en' },
       lean: { virtuals: true },
       sort,
       select,
