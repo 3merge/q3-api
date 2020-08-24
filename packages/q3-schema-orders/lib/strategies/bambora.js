7b19b800d86442d11d8d5cea57385fc32bd8db2a
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2020-08-24 11:35:18 -0400

    [chore] bambora dep upgrades (#92)
    * chore: callback for gateway int
    
    * refactor: changed payment env variable names

--- packages/q3-schema-orders/lib/strategies/bambora.js
+++ packages/q3-schema-orders/lib/strategies/bambora.js
@@ -1,15 +1,22 @@
 const Bambora = require('bambora-node');
 
-module.exports = (doc, token) => {
-  const client = new Bambora(process.env.BAMBORA_API_KEY);
+module.exports = (doc, token, callback) => {
+  const client = new Bambora(
+    process.env.PAYMENT_GATEWAY_KEY,
+    process.env.PAYMENT_GATEWAY_SKIP_PREAUTH,
+  );
+
   const shipping = doc.shipping.normalize().bambora();
   const billing = doc.billing.normalize().bambora();
 
-  return client.postPayment({
-    amount: doc.total,
-    name: `${doc.billing.firstName} ${doc.billing.lastName}`,
-    shipping,
-    billing,
-    token,
-  });
+  return client.postPayment(
+    {
+      amount: doc.total,
+      name: `${doc.billing.firstName} ${doc.billing.lastName}`,
+      shipping,
+      billing,
+      token,
+    },
+    callback,
+  );
 };
