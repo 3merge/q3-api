229ccca66ee791f9fb2cfbcb6f73118bddb1a3c7
Mike <mibberson@3merge.ca>
2021-01-04 11:00:36 -0500

    chore: remove comments in demo

--- demo/index.js
+++ demo/index.js
@@ -1,17 +1,9 @@
 const Q3 = require('q3-api');
 const config = require('./config');
-const { Character } = require('./models');
 
-config
-  .connect()
-  // .then(() =>
-  //   Character.initializeFuzzySearching().then(() => {
-  //     console.log('DONE');
-  //   }),
-  // )
-  .catch((e) => {
-    // eslint-disable-next-line
-    console.log(e);
-  });
+config.connect().catch((e) => {
+  // eslint-disable-next-line
+  console.log(e);
+});
 
 module.exports = Q3.$app;
