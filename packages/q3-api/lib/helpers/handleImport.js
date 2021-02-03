c9d06d4dadf7f0dcd1bee1f9ccfe85a0bf91b198
Mike <mibberson@3merge.ca>
2021-02-03 12:40:17 -0500

    fix: file naming import

--- packages/q3-api/lib/helpers/handleImport.js
+++ packages/q3-api/lib/helpers/handleImport.js
@@ -1,19 +1,25 @@
-const { size } = require('lodash');
+const { size, isObject } = require('lodash');
 const csv = require('fast-csv');
 
 module.exports = (next, headers) => async (
   payload,
   attachments,
-) => {
-  const rows = [];
-  return new Promise((resolve, reject) =>
-    csv
-      .parseStream(attachments.import, {
-        renameHeaders: size(headers) > 0,
-        headers,
-      })
+) =>
+  new Promise((resolve, reject) => {
+    const rows = [];
+
+    if (!isObject(attachments))
+      return reject(new Error('No attachments'));
+
+    return csv
+      .parseStream(
+        attachments[Object.keys(attachments)[0]],
+        {
+          renameHeaders: size(headers) > 0,
+          headers,
+        },
+      )
       .on('error', reject)
       .on('data', rows.push.bind(rows))
-      .on('end', () => resolve(rows)),
-  ).then(next);
-};
+      .on('end', () => resolve(rows));
+  }).then(next);
