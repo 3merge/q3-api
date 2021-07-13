f84047ad3df84b30d1107dab0c8ca2aeb699cd36
Mike <mibberson@3merge.ca>
2021-07-13 13:59:14 -0400

    chore: bulk notification dismss

--- packages/q3-api/lib/routes/reports/get.js
+++ packages/q3-api/lib/routes/reports/get.js
@@ -5,16 +5,17 @@ const {
   setExecutableTemplatePathInRequest,
 } = require('../../helpers');
 
-const ControllerReports = setExecutableTemplateVariablesInRequest(
-  async (req, res) =>
-    res.ok({
-      // eslint-disable-next-line
-      data: await require(get(
-        req,
-        '$executableTemplatePath',
-      ))(get(req, '$query'), get(req, '$session')),
-    }),
-);
+const ControllerReports =
+  setExecutableTemplateVariablesInRequest(
+    async (req, res) =>
+      res.ok({
+        // eslint-disable-next-line
+        data: await require(get(
+          req,
+          '$executableTemplatePath',
+        ))(get(req, '$query'), get(req, '$session')),
+      }),
+  );
 
 ControllerReports.authorization = [verify];
 
---
+++ packages/q3-api/lib/routes/system-notifications/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { compose, verify } = require('q3-core-composer');
+const { compact, get, map, size } = require('lodash');
+const { Notifications } = require('../../models');
+
+const NotificationsPostController = compose(
+  async (req, res) => {
+    const ids = map(
+      compact(get(req, 'body.ids')),
+      mongoose.Types.ObjectId,
+    );
+
+    if (size(ids))
+      await Notifications.updateMany(
+        {
+          _id: { $in: ids },
+          userId: get(req, 'user._id'),
+        },
+        {
+          $set: {
+            hasSeen: true,
+            hasDownloaded: true,
+          },
+        },
+      );
+
+    res.acknowledge();
+  },
+);
+
+NotificationsPostController.authorization = [verify];
+
+module.exports = NotificationsPostController;
