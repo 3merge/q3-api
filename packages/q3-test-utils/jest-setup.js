f2d32cd88d5356c308d99c549064865780e62ada
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2021-12-15 09:10:17 -0500

    test: file import and stream (#157)

--- packages/q3-test-utils/jest-setup.js
+++ packages/q3-test-utils/jest-setup.js
@@ -1,10 +1,10 @@
 require('q3-schema-types');
 
-global.wait = (callback) =>
+global.wait = (callback, interval = 100) =>
   new Promise((r) =>
     // eslint-disable-next-line
     setTimeout(async () => {
       await callback();
       r();
-    }, 50),
+    }, interval),
   );
