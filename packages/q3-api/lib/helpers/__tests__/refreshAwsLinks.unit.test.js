239c3038a8a1afa35758af46972783d3b33975e7
Mike <mibberson@3merge.ca>
2022-10-11 12:58:00 -0400

    refactor: include multiple attributes

--- packages/q3-api/lib/helpers/__tests__/refreshAwsLinks.unit.test.js
+++ packages/q3-api/lib/helpers/__tests__/refreshAwsLinks.unit.test.js
@@ -49,4 +49,30 @@ describe('refreshAwsLinks', () => {
       </p>`,
     );
   });
+
+  it('should replace with private link', () => {
+    aws.getPrivate.mockImplementation((f) =>
+      f.includes('food')
+        ? 'https://cdn.photos/1.png'
+        : 'https://cdn.photos/2.png',
+    );
+
+    const out = refreshAwsLinks(
+      `<p>This is a link
+        <a href="https://private.s3.amazonaws.com/6107eb61033dff002338d28e26/Person%20looking%20at%20food%20in%20the%20fridge.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIAIFFGHKU2344432YAIA%2F20210802%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20210802T130133Z&amp;X-Amz-Expires=86400&amp;X-Amz-Signature=b71d599fcf05750110a523a2345454332e8fa4bf4b4a87592ff254281&amp;X-Amz-SignedHeaders=host">Food</a>
+        <a href="https://private.s3.amazonaws.com/61a644b75aedec002a546e68/blob?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIFFGHKUY4Q2LYAIA%2F20211203%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20211203T164354Z&X-Amz-Expires=86400&X-Amz-Signature=273898788b79e08f95dd60fd1278892fbd3c11bb23cf8b4e0dd3841ef1f1d1da&X-Amz-SignedHeaders=host">Non-food</a>
+      </p>`,
+    );
+
+    expect(aws.getPrivate).toHaveBeenCalledWith(
+      '6107eb61033dff002338d28e26/Person looking at food in the fridge.jpg',
+    );
+
+    expect(out).toMatch(
+      `<p>This is a link
+        <a href="https://cdn.photos/1.png">Food</a>
+        <a href="https://cdn.photos/2.png">Non-food</a>
+      </p>`,
+    );
+  });
 });
--- packages/q3-api/lib/helpers/refreshAwsLinks.js
+++ packages/q3-api/lib/helpers/refreshAwsLinks.js
@@ -3,34 +3,45 @@ const aws = require('../config/aws');
 
 module.exports = (item) => {
   const a = aws();
-  let copy = item;
 
-  try {
-    const replacements = copy.match(
-      new RegExp(/src="(?:[^'"]*)"/, 'gm'),
-    );
+  const execOnAttribute = (attributeName, str) => {
+    let copy = str;
 
-    forEach(replacements, (link) => {
-      try {
-        const [bucket] = link.match(
-          new RegExp(/.[a-z]+\/(.+)\?/, 'gm'),
-        );
+    try {
+      const replacements = copy.match(
+        new RegExp(`${attributeName}="(?:[^'"]*)"`, 'gm'),
+      );
 
-        const m = a.getPrivate(
-          decodeURIComponent(
-            bucket.replace(/(\?|\.[a-z]+\/)/gm, ''),
-          ).replace(/%20/g, ' '),
-        );
+      forEach(replacements, (link) => {
+        try {
+          const [bucket] = link.match(
+            new RegExp(/.[a-z]+\/(.+)\?/, 'gm'),
+          );
 
-        // eslint-disable-next-line
-        copy = copy.replace(link, `src="${m}"`);
-      } catch (e) {
-        // noop
-      }
-    });
+          const m = a.getPrivate(
+            decodeURIComponent(
+              bucket.replace(/(\?|\.[a-z]+\/)/gm, ''),
+            ).replace(/%20/g, ' '),
+          );
 
-    return copy;
-  } catch (e) {
-    return copy;
-  }
+          // eslint-disable-next-line
+          copy = copy.replace(
+            link,
+            `${attributeName}="${m}"`,
+          );
+        } catch (e) {
+          // noop
+        }
+      });
+
+      return copy;
+    } catch (e) {
+      return copy;
+    }
+  };
+
+  return ['href', 'src'].reduce(
+    (acc, curr) => execOnAttribute(curr, acc),
+    item,
+  );
 };
