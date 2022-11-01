fada0886706d27e910f851c22685c89b8fc9b5c7
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2022-11-01 12:09:38 -0400

    refactor: report and io auth check (#193)

--- packages/q3-api/lib/routes/io/post.js
+++ packages/q3-api/lib/routes/io/post.js
@@ -1,44 +1,19 @@
-const { find, get, isObject } = require('lodash');
-const { exception } = require('q3-core-responder');
+const { get } = require('lodash');
 const Scheduler = require('q3-core-scheduler');
 const { compose } = require('q3-core-composer');
-const {
-  findFileTraversingUpwards,
-} = require('q3-schema-utils');
 const aws = require('../../config/aws');
 const {
+  checkAccessByFileNameAndRoleType,
   setExecutableTemplateVariablesInRequest,
   setExecutableTemplatePathInRequest,
 } = require('../../helpers');
 
-const checkIoSettingsByRoleType = (
-  settings,
-  userRoleType = 'Public',
-) => {
-  if (
-    isObject(settings) &&
-    Array.isArray(settings.role) &&
-    !settings.role.includes(userRoleType)
-  )
-    exception('Authorization')
-      .msg('cannotQueueThisChore')
-      .throw();
-};
-
 const ControllerIo =
   setExecutableTemplateVariablesInRequest(
     async (req, res) => {
-      const settings = find(
-        findFileTraversingUpwards(
-          get(req, 'app.locals.location'),
-          'q3-access-chores.json',
-        ),
-        (item) => item.name === get(req, 'query.template'),
-      );
-
-      checkIoSettingsByRoleType(
-        settings,
-        get(req, 'user.role'),
+      const settings = checkAccessByFileNameAndRoleType(
+        req,
+        'q3-access-chores.json',
       );
 
       await Scheduler.queue(
--- packages/q3-api/lib/routes/reports/get.js
+++ packages/q3-api/lib/routes/reports/get.js
@@ -1,20 +1,27 @@
 const { get } = require('lodash');
 const { compose, verify } = require('q3-core-composer');
 const {
+  checkAccessByFileNameAndRoleType,
   setExecutableTemplateVariablesInRequest,
   setExecutableTemplatePathInRequest,
 } = require('../../helpers');
 
 const ControllerReports =
   setExecutableTemplateVariablesInRequest(
-    async (req, res) =>
+    async (req, res) => {
+      checkAccessByFileNameAndRoleType(
+        req,
+        'q3-access-reports.json',
+      );
+
       res.ok({
         // eslint-disable-next-line
         data: await require(get(
           req,
           '$executableTemplatePath',
         ))(get(req, '$query'), get(req, '$session')),
-      }),
+      });
+    },
   );
 
 ControllerReports.authorization = [verify];
